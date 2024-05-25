25fff7d7ce98100bdcbc1fa5d6330313278cc4fa
Yusif <guliyev.yusif@yandex.ru>
2024-05-25 23:24:55 +0400

    testing shared preference app on android oc

---
+++ app/src/main/java/com/isim/storingdata/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.isim.storingdata
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.isim.storingdata.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    lateinit var sharedPref: SharedPreferences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+//        add(view)
+//        delete(view)
+
+        //SharedPreferences - XML - Key - Value
+        sharedPref = this.getSharedPreferences("com.isim.storingdata", Context.MODE_PRIVATE)
+
+        val userAgePref = sharedPref.getInt("age", -1)
+        if (userAgePref == -1) {
+            binding.txtAge.text = "Your Age: "
+        } else {
+            binding.txtAge.text = "Your Age: ${userAgePref}"
+        }
+    }
+
+    fun add(viwe: View) {
+        binding.button1.setOnClickListener {
+            val myAge = binding.edtText.text.toString().toIntOrNull()
+
+            if (myAge != null) {
+                binding.txtAge.text = "Your age: ${myAge}"
+                sharedPref.edit().putInt("age", myAge).apply() //apply()-значит сохранить данные
+            }
+
+        }
+    }
+
+    fun delete(view: View) {
+        binding.button2.setOnClickListener {
+            val userAgePref = sharedPref.getInt("age", -1)
+            if (userAgePref != -1) {
+                sharedPref.edit().remove("age").apply()
+                binding.txtAge.text = "Your age: "
+            }
+        }
+    }
+}
